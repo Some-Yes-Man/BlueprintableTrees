5e26d515840cd68fda9ab0970cb0242bfad5c792
rkrausse <robert.krausse@web.de>
2020-08-14 21:54:38 +0200

    initial code commit

---
+++ control.lua
@@ -0,0 +1,66 @@
+require 'util'
+
+local avg_grow_time = 60 * 60 * 5 --5 minutes, until a random modifier is applied
+local max_grow_swing = 60 * 60 * 1.5 --1.5 minutes either way
+local tree_types = {
+    'tree-01',
+    'tree-02',
+    'tree-02-red',
+    'tree-03',
+    'tree-04',
+    'tree-05',
+    'tree-06'
+}
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+    if event.created_entity.name == 'tree-sapling' then
+        --Get a random grow tick
+        local grow_tick = (avg_grow_time or 60 * 60 * 5) + math.random(-max_grow_swing, max_grow_swing) + event.tick
+        --Get or create the global tick to grow on
+        global.growing_times[grow_tick] = global.growing_times[grow_tick] or {}
+        --local reference because speed and things
+        local sapling = global.growing_times[grow_tick]
+        sapling[#sapling + 1] = { entity = event.created_entity }
+    end
+end)
+
+script.on_event(defines.events.on_built_entity, function(event)
+    if event.created_entity.name == 'tree-sapling' then
+        --Get a random grow tick
+        local grow_tick = (avg_grow_time or 60 * 60 * 5) + math.random(-max_grow_swing, max_grow_swing) + event.tick
+        --Get or create the global tick to grow on
+        global.growing_times[grow_tick] = global.growing_times[grow_tick] or {}
+        --local reference because speed and things
+        local sapling = global.growing_times[grow_tick]
+        sapling[#sapling + 1] = { entity = event.created_entity }
+    end
+end)
+
+-- Grow trees every few ticks
+script.on_event(defines.events.on_tick, function(event)
+    --Is there a sapling ready to grow this tick
+    if global.growing_times[event.tick] then
+        --Loop through all saplings ready to grow this tick
+        for _, sapling in ipairs(global.growing_times[event.tick]) do
+            --If the sapling is still valid then make tree!
+            if sapling and sapling.entity and sapling.entity.valid then
+                local newtree = tree_types[math.random(1, #tree_types)]
+                sapling.entity.surface.create_entity { name = newtree, position = sapling.entity.position }
+                sapling.entity.destroy()
+            end
+        end
+        --Remove the data from the table
+        global.growing_times[event.tick] = nil
+    end
+end)
+
+local function initSimpleSaplings()
+    -- Create the growing times table
+    -- If something here is changed between releases you'll get a weird error
+    -- https://github.com/Blu3wolf/Treefarm-Lite/issues/60 they're had a similar issue
+    global.growing_times = global.growing_times or {}
+end
+
+script.on_init(initSimpleSaplings)
+
+script.on_configuration_changed(initSimpleSaplings)
---
+++ data.lua
@@ -0,0 +1,57 @@
+data:extend({
+    -- Entity ----------------------------------------------------
+    {
+        type = "container",
+        name = "tree-sapling",
+        inventory_size = 0,
+        icon = "__My-Tree-Mod-Update__/graphics/tree-sapling-icon.png",
+        flags = { "player-creation", "placeable-neutral", "breaths-air", "not-repairable" },
+        icon_size = 32,
+        minable = {
+            hardness = 0.1,
+            mining_time = 0.5,
+            result = "tree-sapling"
+        },
+        max_health = 20,
+        corpse = "small-remnants",
+        collision_box = { { -0.2, -0.2 }, { 0.2, 0.2 } },
+        selection_box = { { -0.4, -0.4 }, { 0.4, 0.4 } },
+        vehicle_impact_sound = {
+            filename = "__base__/sound/car-wood-impact.ogg",
+            volume = 1.0
+        },
+        picture = {
+            filename = "__My-Tree-Mod-Update__/graphics/tree-sapling.png",
+            priority = "extra-high",
+            width = 32,
+            height = 42,
+            shift = { 0.0, -0.25 }
+        }
+    },
+    -- Item ------------------------------------------------------
+    {
+        type = "item",
+        name = "tree-sapling",
+        icon = "__My-Tree-Mod-Update__/graphics/tree-sapling-icon.png",
+        flags = {},
+        icon_size = 32,
+        subgroup = "raw-material",
+        order = "a[tree-sapling]",
+        place_result = "tree-sapling",
+        stack_size = 200
+    },
+    -- Recipe ----------------------------------------------------
+    {
+        type = "recipe",
+        name = "tree-sapling",
+        category = "crafting-with-fluid",
+        enabled = true,
+        ingredients = {
+            { "wood", 1 },
+            { type = "fluid", name = "water", amount = 20 }
+        },
+        result_count = 1,
+        result = "tree-sapling"
+    }
+
+})
